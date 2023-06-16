d19c733bfc0c169eeac7be66af621cfc9bd5e5a2
Artemiy <artemiy.borodulin@gmail.com>
2023-06-16 21:17:36 +0500

    station_names added

--- home/views.py
+++ home/views.py
@@ -12,20 +12,20 @@ perm = {
 }
 
 
-def get_bus_schedule(lat, lng):
+def index(request):
+    lat = perm['lat']
+    lng = perm['lng']
     url = f'https://api.rasp.yandex.net/v3.0/nearest_stations/?apikey={api_key}&format=json&lat={lat}&lng={lng}&distance=30&lang=ru_RU&limit=1000'
     response = requests.get(url)
-    return response.json()
+    data = response.json()
 
+    with open('last_request.json', 'w') as f:
+        f.write(json.dumps(data, indent=5, ensure_ascii=False))
 
-data = get_bus_schedule(perm['lat'], perm['lng'])
-
-with open('last_request.json', 'w') as f:
-    f.write(json.dumps(data, indent=5, ensure_ascii=False))
-
-number_of_stations = data['pagination']['total']
-for i in range(number_of_stations):
-    if data['stations'][i]['station_type'] == 'bus_stop':
-        print(data['stations'][i]['title'])
-
-# json.dumps(data, indent=5, ensure_ascii=False)
+    station_names = []
+    number_of_stations = data['pagination']['total']
+    for i in range(number_of_stations):
+        if data['stations'][i]['station_type'] == 'bus_stop':
+            station_names.append(data['stations'][i]['title'])
+    json.dumps(data, indent=5, ensure_ascii=False)
+    return render(request, "index.html", {'station_names': station_names})
